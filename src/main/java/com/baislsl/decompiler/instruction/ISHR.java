d15c6a492fea874ece440f046899204711c78a2e
baislsl <baislsl666@gmail.com>
2017-10-09 21:53:10 +0800

    ishl, ishr, iushr

---
+++ src/main/java/com/baislsl/decompiler/instruction/ISHR.java
@@ -0,0 +1,12 @@
+package com.baislsl.decompiler.instruction;
+
+import com.baislsl.decompiler.engine.Frame;
+import com.baislsl.decompiler.structure.attribute.Code;
+
+public class ISHR extends OperationInstruction {
+    @Override
+    public Executable build(Code code, Frame frame) {
+        this.operator = "<<";
+        return super.build(code, frame);
+    }
+}
