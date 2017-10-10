f1cfbe89510a986da8fcc911b253c1ce673753a2
baislsl <baislsl666@gmail.com>
2017-10-10 19:30:20 +0800

    update istore, new

--- src/main/java/com/baislsl/decompiler/instruction/NEW.java
+++ src/main/java/com/baislsl/decompiler/instruction/NEW.java
@@ -3,7 +3,7 @@ package com.baislsl.decompiler.instruction;
 import com.baislsl.decompiler.DecompileException;
 
 public class NEW extends Instruction {
-
+    // TODO
     @Override
     public void exec() throws DecompileException {
         System.out.println("use instruction " + this.getClass().getName());
